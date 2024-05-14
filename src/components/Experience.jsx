5b86dd8f8a067d34109d608371b2d9469c459f0c
Taha Shajarian <t.shajarian@pdnco>
2024-05-14 18:02:52 +0330

    first commit

---
+++ src/components/Experience.jsx
@@ -0,0 +1,36 @@
+import { OrbitControls } from "@react-three/drei";
+import Woman from "./Woman";
+import Taha from "./Taha";
+
+const Experience = () => {
+  return (
+    <>
+      <OrbitControls />
+      <ambientLight />
+      <directionalLight
+        position={[-5, 5, 5]}
+        castShadow
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+      />
+      {/* <group position={[0, -1, 0]}>
+        <Woman />
+      </group> */}
+
+      <group position={[0, -1, 0]}>
+        <Taha />
+      </group>
+
+      <mesh
+        rotation={[-0.5 * Math.PI, 0, 0]}
+        position={[0, -1, 0]}
+        receiveShadow
+      >
+        <planeBufferGeometry args={[10, 10, 10, 10]} />
+        <meshStandardMaterial />
+      </mesh>
+    </>
+  );
+};
+
+export default Experience;
