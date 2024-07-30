71685a00fc2d6de9b1a1b06a22ad9d6e0de0adf0
Taha <shajariantaha@gmail.com>
2024-07-30 22:54:21 +0330

    bug fix

--- src/components/Lights.jsx
+++ src/components/Lights.jsx
@@ -32,9 +32,9 @@ const Lights = () => {
       {/* <group position={[-5.5, 0, 5.5]}>
         <Lamp />
       </group> */}
-      <group position={[5.5, 0, 5.5]}>
+      {/* <group position={[5.5, 0, 5.5]}>
         <Lamp />
-      </group>
+      </group> */}
     </>
   );
 };
