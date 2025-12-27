f6b35a7156327b5249fc694f4bb43bcea8de23b4
Taha <taha.s@bitazza.com>
2025-12-28 00:13:28 +0330

    make hands animated

--- src/components/StuffOnWall/GodsHand.jsx
+++ src/components/StuffOnWall/GodsHand.jsx
@@ -1,16 +1,64 @@
 import { useLoader } from "@react-three/fiber";
+import { useSpring, animated } from "@react-spring/three";
 import React, { useRef } from "react";
 import { TextureLoader } from "three";
 
 const GodsHand = () => {
-  const ref = useRef();
-  const texture = useLoader(TextureLoader, "/textures/gods-hand.png");
+  const adamHandTex = useLoader(TextureLoader, "/textures/adam-hand.png");
+  const godHandTex = useLoader(TextureLoader, "/textures/god-hand.png");
+
+  const isAnimating = useRef(false);
+
+  const [{ leftX, rightX }, api] = useSpring(() => ({
+    leftX: -1.3,
+    rightX: 1.3,
+  }));
+
+  const handleClick = () => {
+    if (isAnimating.current) return;
+    isAnimating.current = true;
+
+    api.start({
+      leftX: -0.975,
+      rightX: 0.975,
+      config: {
+        tension: 60,
+        friction: 80,
+      },
+      onRest: () => {
+        api.start({
+          leftX: -1.3,
+          rightX: 1.3,
+          config: {
+            tension: 60,
+            friction: 50,
+          },
+          onRest: () => {
+            isAnimating.current = false;
+          },
+        });
+      },
+    });
+  };
 
   return (
-    <mesh position={[0, 0, 0]} ref={ref}>
-      <planeGeometry args={[4, 3]} />
-      <meshBasicMaterial map={texture} transparent />
-    </mesh>
+    <group>
+      {/* God hand */}
+      <animated.mesh position-x={rightX} onClick={handleClick}>
+        <planeGeometry args={[2, 1.5]} />
+        <meshBasicMaterial map={godHandTex} transparent />
+      </animated.mesh>
+
+      {/* Adam hand */}
+      <animated.mesh
+        position-x={leftX}
+        rotation={[0, 0, 0.2]}
+        onClick={handleClick}
+      >
+        <planeGeometry args={[2, 1.5]} />
+        <meshBasicMaterial map={adamHandTex} transparent />
+      </animated.mesh>
+    </group>
   );
 };
 
