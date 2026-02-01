3f43de271b4faf8a8540066fa6417610b8ad789a
Taha <shajariantaha@gmail.com>
2026-02-02 02:21:50 +0330

    bug fix

--- src/components/UI/ArrowControls.jsx
+++ src/components/UI/ArrowControls.jsx
@@ -1,26 +1,32 @@
-import React from "react";
-import { useArrowsStore } from "../../stores/useArrowStore";
-
+import React from "react"
+import { useArrowsStore } from "../../stores/useArrowStore"
 
 const ArrowButton = ({ direction, symbol }) => {
-  const setArrow = useArrowsStore((s) => s.setArrow);
+  const setArrow = useArrowsStore((s) => s.setArrow)
 
   return (
     <button
+      type="button"
+      style={{ touchAction: "manipulation" }}
       className="select-none font-bold w-[30px] h-[30px] rounded-md shadow-lg transform transition-transform duration-200 active:scale-90 text-3xl flex items-center justify-center"
       onMouseDown={() => setArrow(direction, true)}
       onMouseUp={() => setArrow(direction, false)}
       onMouseLeave={() => setArrow(direction, false)}
-      onTouchStart={() => setArrow(direction, true)}
-      onTouchEnd={() => setArrow(direction, false)}
+      onTouchStart={(e) => {
+        e.preventDefault()
+        setArrow(direction, true)
+      }}
+      onTouchEnd={(e) => {
+        e.preventDefault()
+        setArrow(direction, false)
+      }}
     >
       {symbol}
     </button>
-  );
-};
+  )
+}
 
 const ArrowControls = () => {
-
   return (
     <div className="absolute bottom-10 left-10 flex flex-col items-center space-y-2 select-none">
       <ArrowButton direction="forward" symbol="⬆️" />
@@ -30,7 +36,7 @@ const ArrowControls = () => {
         <ArrowButton direction="right" symbol="➡️" />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ArrowControls;
+export default ArrowControls
